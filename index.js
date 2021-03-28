e2da8c7ba2ef6e16cb66103c31f185be1d7d085a
Sam <samara.amanda01@gmail.com>
2021-03-28 17:41:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'YOUR-NAME is the champion'
