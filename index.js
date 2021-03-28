133a2ab775543b5ed2306eedc5a00ffb5c82ded6
Sam <samara.amanda01@gmail.com>
2021-03-28 17:50:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Write your code here!
 main.remove();
 
-let newHeader = document.querySelector("h1#victory")
+let newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
 newHeader.innerHTML = 'YOUR-NAME is the champion'
